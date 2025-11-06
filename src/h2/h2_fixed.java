cb1ac53b8b963c7b1e17c272baa1e11141e46f97
Maxi <maximilian.s107@evt.schule.koeln>
2025-11-06 19:40:14 +0100

    Add fixed version of H2

---
+++ src/h2/h2_fixed.java
@@ -0,0 +1,26 @@
+//by mschro67
+
+package h2;
+
+public class h2_main{
+    public static void main(String[] args){
+        int i=3,j=67,k=21;
+        System.out.println("i: "+i+", j: "+j+", k: "+k);
+        int min, max;
+        min=i;
+        max=i;
+        if (j<min){
+            min=j;
+        }
+        if (k<min){
+            min=j;
+        }
+        if (j>max){
+            max=j;
+        }
+        if (k>max){
+            max=k;
+        }
+        System.out.println("min: "+min+", max: "+max);
+    }
+}
